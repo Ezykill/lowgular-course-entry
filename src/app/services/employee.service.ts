feae1901888b44df14a776043b10e3c5b1f74431
Igor Moryto <56959606+Ezykill@users.noreply.github.com>
2022-11-25 16:39:36 +0100

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {map, Observable} from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
+import { ApiResponse } from './api.response';
+import { EmployeeResponse } from './employee.response';
 import { CreateEmployeeModel } from '../model/create-employee.model';
-import {ApiResponse} from "./api.response";
-import {EmployeeResponse} from "./employee.response";
-
+import {EmployeeModel} from "../model/employee.model";
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getAll(): Observable<PersonModel[]>
-  {
-  return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
+  getAll(): Observable<PersonModel[]> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>('https://dummy.restapiexample.com/api/v1/employees').pipe(
       map((response: ApiResponse<EmployeeResponse[]>): PersonModel[] => {
         return response.data.map((employeeResponse: EmployeeResponse) => {
           return {
@@ -35,9 +35,20 @@ export class EmployeeService {
       ;
   }
 
-  delete(id: string): Observable<void>
-  {
+  delete(id: string): Observable<void> {
     return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/' + id).pipe(map(_ => void 0))
       ;
+  }
+
+  getOne(id: string): Observable<EmployeeModel> {
+    return this._httpClient.get<ApiResponse<EmployeeResponse>>('https://dummy.restapiexample.com/api/v1/employees/'+id).pipe(
+      map((response: ApiResponse<EmployeeResponse>): EmployeeModel => ({
+        id: response.data.id,
+        image: response.data.profile_image,
+        email: '',
+        name: response.data.employee_name
+      }))
+    );
+
   }
 }
